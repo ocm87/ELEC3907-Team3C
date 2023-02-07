4603b7881707faa6441b656120f0b79f19c18c06
Owen MacIntosh <ocm87@yahoo.ca>
2023-02-07 16:55:38 -0500

    added notes to the file

--- python_http/data_get.py
+++ python_http/data_get.py
@@ -5,11 +5,19 @@ ARDUINO_IP = "172.20.10.10"
 request_on = "http://" + ARDUINO_IP + "/13/on"
 request_off = "http://" + ARDUINO_IP + "/13/off"
 
-on = requests.get(request_on)
+# Some notes on the various requests
+"""
+POST requests for enabling the sos alarm remotly
+GET request for reading sensor data
+
+GET reqests data into mySQL database for storage
+"""
+
+on = requests.post(request_off)
 
 off = ""
 
-print(on.text)
+print(on)
 
 
 
