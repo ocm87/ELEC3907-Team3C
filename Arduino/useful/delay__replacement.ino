375204fae6961ecb9c52485e6ad3edf528a2ab0f
Arielwoll <91855900+Arielwoll@users.noreply.github.com>
2023-03-07 15:01:38 -0500

    Add files via upload

---
+++ Arduino/useful/delay__replacement.ino
@@ -0,0 +1,14 @@
+unsigned long time_now= 0;
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(millis()>=time_now + 1000){  // 1000 will be changed based on dealy we want
+    time_now+= millis();
+  }
+  }
+
+
+// the if statement will replace delay() will have to copy code each time 
